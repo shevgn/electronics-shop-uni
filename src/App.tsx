9ed24ffdaf7b10f9c44f8a8318fdcadd1d2fdb9e
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2024-11-27 18:55:27 +0200

    Login route added

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import About from "./pages/About";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
