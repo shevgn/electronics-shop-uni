e3761f37e898f0967c9d536cb355b2743d2b8d28
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2024-11-28 20:02:46 +0200

    import AuthPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import AuthPage from "./pages/AuthPage/AuthPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<AuthPage />} />
       </Routes>
     </Router>
   );
