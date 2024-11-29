86fd4e5ec427cb2403ca5701f53d38d097099f78
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2024-11-29 20:23:13 +0200

    id and text image added

--- src/components/CatalogBlock.tsx
+++ src/components/CatalogBlock.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import CatalogItem from "./CatalogItem";
 import CatalogPagination from "./CatalogPagination";
 import { TCatalogItem } from "../types/CatalogItem";
@@ -8,78 +7,91 @@ export default function CatalogBlock() {
 
   const items: TCatalogItem[] = [
     {
+      id: 1,
       name: "Apple iPhone 12 Pro Max",
       price: "$1,099.00",
       category: "For Phone",
-      image: "/iphone.jpg",
+      image: "../../public/ps5.webp",
     },
     {
+      id: 2,
       name: "Sony PlayStation 5",
       price: "$499.00",
       category: "For Gaming",
-      image: "/playstation.jpg",
+      image: "../../public/ps5-wide.webp",
     },
     {
+      id: 3,
       name: "Apple HomePod Mini",
       price: "$99.00",
       category: "For Home",
       image: "/homepod.jpg",
     },
     {
+      id: 4,
       name: "Sony WH-1000XM4",
       price: "$349.00",
       category: "For Music",
       image: "/headphones.jpg",
     },
     {
+      id: 5,
       name: "Apple iPhone 12 Pro Max",
       price: "$1,099.00",
       category: "For Phone",
       image: "/iphone.jpg",
     },
     {
+      id: 6,
       name: "Sony PlayStation 5",
       price: "$499.00",
       category: "For Gaming",
       image: "/playstation.jpg",
     },
     {
+      id: 7,
       name: "Apple HomePod Mini",
       price: "$99.00",
       category: "For Home",
       image: "/homepod.jpg",
     },
     {
+      id: 8,
       name: "Sony WH-1000XM4",
       price: "$349.00",
       category: "For Music",
       image: "/headphones.jpg",
     },
     {
+      id: 9,
       name: "Apple iPhone 12 Pro Max",
       price: "$1,099.00",
       category: "For Phone",
       image: "/iphone.jpg",
     },
     {
+      id: 10,
       name: "Sony PlayStation 5",
       price: "$499.00",
       category: "For Gaming",
       image: "/playstation.jpg",
     },
     {
+      id: 11,
       name: "Apple HomePod Mini",
       price: "$99.00",
       category: "For Home",
       image: "/homepod.jpg",
     },
     {
+      id: 12,
       name: "Sony WH-1000XM4",
       price: "$349.00",
       category: "For Music",
       image: "/headphones.jpg",
     },
     {
+      id: 13,
       name: "Apple iPhone 12 Pro Max",
       price: "$1,099.00",
       category: "For Phone",
