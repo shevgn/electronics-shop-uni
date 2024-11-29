f5e420610f59a2c18976ce624dca4a2e1586031c
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2024-11-29 20:19:56 +0200

    animations for auth page

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { AnimatePresence } from "motion/react";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
+import AuthMain from "./AuthMain";
 
 export default function AuthPage() {
   const [isLogin, setIsLogin] = useState<boolean | null>(null);
@@ -29,62 +31,11 @@ export default function AuthPage() {
           />
         </svg>
       </Link>
-      <section
-        className={`relative flex h-full w-full flex-col items-center justify-center rounded-lg bg-white p-4 font-semibold text-black shadow-lg transition-all sm:h-2/3 sm:w-2/3 ${isLogin === null && "space-y-4"}`}
-      >
-        {isLogin !== null && (
-          <button
-            onClick={() => setIsLogin(null)}
-            className="absolute right-4 top-0 rounded-b-full p-2 shadow transition-all hover:pt-4"
-            type="button"
-          >
-            <svg
-              className="h-8 w-8 rotate-0"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 5V19"
-                stroke="#000000"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M5 12L12 19L19 12"
-                stroke="#000000"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-            <span className="visually-hidden">Close</span>
-          </button>
-        )}
-        {isLogin === true ? (
-          <LoginForm />
-        ) : isLogin === false ? (
-          <RegisterForm />
-        ) : (
-          <>
-            <button
-              onClick={() => setIsLogin(true)}
-              className="w-2/3 rounded-lg border border-black bg-black p-4 text-white transition-all hover:scale-105 sm:w-1/3 sm:p-3"
-              type="button"
-            >
-              <span>Login</span>
-            </button>
-            <button
-              onClick={() => setIsLogin(false)}
-              className="w-2/3 rounded-lg border border-black bg-white p-4 transition-all hover:scale-105 sm:w-1/3 sm:p-3"
-              type="button"
-            >
-              <span>Sign Up</span>
-            </button>
-          </>
-        )}
-      </section>
+      <AnimatePresence mode="wait">
+        {isLogin === true && <LoginForm setIsLogin={setIsLogin} />}
+        {isLogin === false && <RegisterForm setIsLogin={setIsLogin} />}
+        {isLogin === null && <AuthMain setIsLogin={setIsLogin} />}
+      </AnimatePresence>
     </main>
   );
 }
