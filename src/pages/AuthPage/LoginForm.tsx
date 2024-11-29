df5cd721e1f573f96403e2d07440d4ae53e5040d
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2024-11-29 20:19:12 +0200

    form use animated section

--- src/pages/AuthPage/LoginForm.tsx
+++ src/pages/AuthPage/LoginForm.tsx
@@ -1,8 +1,13 @@
 import InputField from "../../components/InputField";
+import MSection from "./MSection";
 
-export default function LoginForm() {
+export default function LoginForm({
+  setIsLogin,
+}: {
+  setIsLogin: (value: null) => void;
+}) {
   return (
-    <>
+    <MSection key={"login"}>
       <h2 className="p-2 text-4xl">Login</h2>
       <form className="my-10 flex w-full flex-col space-y-4 md:w-1/2 lg:w-1/3">
         <InputField label={"Email"} id={"email"} type={"email"} />
@@ -14,6 +19,34 @@ export default function LoginForm() {
           <span>Confirm</span>
         </button>
       </form>
-    </>
+      <button
+        onClick={() => setIsLogin(null)}
+        className="absolute right-4 top-0 rounded-b-full p-2 shadow transition-all hover:pt-4"
+        type="button"
+      >
+        <svg
+          className="h-8 w-8 rotate-0"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12 5V19"
+            stroke="#000000"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M5 12L12 19L19 12"
+            stroke="#000000"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        <span className="visually-hidden">Close</span>
+      </button>
+    </MSection>
   );
 }
--- src/pages/AuthPage/RegisterForm.tsx
+++ src/pages/AuthPage/RegisterForm.tsx
@@ -1,8 +1,13 @@
 import InputField from "../../components/InputField";
+import MSection from "./MSection";
 
-export default function RegisterForm() {
+export default function RegisterForm({
+  setIsLogin,
+}: {
+  setIsLogin: (value: null) => void;
+}) {
   return (
-    <>
+    <MSection key={"register"}>
       <h2 className="p-2 text-4xl">Create Account</h2>
       <form className="my-10 flex w-full flex-col space-y-4 md:w-1/2 lg:w-1/3">
         <InputField label={"Name"} id={"name"} type={"text"} />
@@ -20,6 +25,34 @@ export default function RegisterForm() {
           <span>Confirm</span>
         </button>
       </form>
-    </>
+      <button
+        onClick={() => setIsLogin(null)}
+        className="absolute right-4 top-0 rounded-b-full p-2 shadow transition-all hover:pt-4"
+        type="button"
+      >
+        <svg
+          className="h-8 w-8 rotate-0"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12 5V19"
+            stroke="#000000"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M5 12L12 19L19 12"
+            stroke="#000000"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        <span className="visually-hidden">Close</span>
+      </button>
+    </MSection>
   );
 }
