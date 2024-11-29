3a8da44b426ab0e89c9c9a167777a3cea5c21856
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2024-11-29 20:21:17 +0200

    new path to auth forms

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import AuthPage from "./pages/AuthPage/AuthPage";
+import Product from "./pages/Product";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<AuthPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/product/:id" element={<Product />} />
       </Routes>
     </Router>
   );
