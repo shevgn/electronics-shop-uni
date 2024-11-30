2dec14ceeb3e5666e802abb82605d98eb2ff2b68
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2024-11-30 21:06:13 +0200

    interactions added

--- src/components/AccountPopover.tsx
+++ src/components/AccountPopover.tsx
@@ -7,6 +7,11 @@ import {
   offset,
   flip,
   shift,
+  FloatingFocusManager,
+  useDismiss,
+  useRole,
+  useClick,
+  useInteractions,
 } from "@floating-ui/react";
 import { motion, AnimatePresence } from "framer-motion";
 
@@ -29,6 +34,16 @@ export default function UserPopover() {
     right: { translateX: -10 },
   }[placement.split("-")[0]];
 
+  const click = useClick(context);
+  const dismiss = useDismiss(context);
+  const role = useRole(context);
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    click,
+    dismiss,
+    role,
+  ]);
+
   const handleClick = () => {
     if (isLoggedIn) {
       setIsOpen((prev) => !prev);
@@ -42,6 +57,7 @@ export default function UserPopover() {
       <button
         title="Account"
         ref={refs.setReference}
+        {...getReferenceProps()}
         onClick={handleClick}
         className="flex h-full w-full items-center justify-center rounded-full border border-gray-200 transition-all hover:scale-105 hover:border-gray-300 active:scale-95"
       >
@@ -56,9 +72,9 @@ export default function UserPopover() {
               <path
                 d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z"
                 stroke="#000000"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
             </svg>
             <span className="sr-only">Account</span>
@@ -68,45 +84,48 @@ export default function UserPopover() {
         )}
       </button>
 
-      <FloatingPortal>
-        <AnimatePresence>
-          {isOpen && (
-            <motion.div
-              initial={{
-                opacity: 0,
-                ...translate,
-              }}
-              animate={{
-                opacity: 1,
-                translateX: 0,
-                translateY: 0,
-              }}
-              exit={{ opacity: 0, ...translate }}
-              transition={{ duration: 0.3 }}
-              ref={refs.setFloating}
-              style={{
-                position: strategy,
-                top: y ?? 0,
-                left: x ?? 0,
-              }}
-              className="rounded-lg border border-gray-200 bg-white p-4 text-black shadow-lg"
-            >
-              <h3 className="text-lg font-bold">User Info</h3>
-              <p className="text-sm">Name: John Doe</p>
-              <p className="text-sm">Email: john.doe@example.com</p>
-              <button
-                className="mt-2 rounded bg-red-500 px-4 py-2 text-sm text-white hover:bg-red-600"
-                onClick={() => {
-                  setIsLoggedIn(false);
-                  setIsOpen(false);
+      <AnimatePresence>
+        {isOpen && (
+          <FloatingPortal>
+            <FloatingFocusManager context={context}>
+              <motion.section
+                initial={{
+                  opacity: 0,
+                  ...translate,
                 }}
+                animate={{
+                  opacity: 1,
+                  translateX: 0,
+                  translateY: 0,
+                }}
+                exit={{ opacity: 0, ...translate }}
+                transition={{ duration: 0.3 }}
+                ref={refs.setFloating}
+                {...getFloatingProps()}
+                style={{
+                  position: strategy,
+                  top: y ?? 0,
+                  left: x ?? 0,
+                }}
+                className="rounded-lg border bg-white p-4 text-black shadow-lg"
               >
-                Log Out
-              </button>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </FloatingPortal>
+                <h3 className="mb-2 text-lg font-bold">User Info</h3>
+                <p className="text-sm">Name: John Doe</p>
+                <p className="text-sm">Email: john.doe@example.com</p>
+                <button
+                  className="mt-3 rounded-md bg-red-500 px-4 py-2 text-sm text-white hover:bg-red-600"
+                  onClick={() => {
+                    setIsLoggedIn(false);
+                    setIsOpen(false);
+                  }}
+                >
+                  Log Out
+                </button>
+              </motion.section>
+            </FloatingFocusManager>
+          </FloatingPortal>
+        )}
+      </AnimatePresence>
     </>
   );
 }
