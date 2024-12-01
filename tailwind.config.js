b21d93cf69c357ed7edee3553d30201fce4298fd
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2024-12-01 16:27:33 +0200

    tailwind-scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+import scrollbar from "tailwind-scrollbar";
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [scrollbar],
 };
