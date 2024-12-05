7b8f5e1e0a60df427cfa83a7cf682733834ee2a5
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2024-12-05 20:31:43 +0200

    product type added

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -38,3 +38,14 @@ export type LoginResponse = {
   };
   token: string;
 };
+
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+  release_year: number;
+  categories: string[];
+  details: Record<string, string>;
+  images: string[];
+  brand: string;
+};
