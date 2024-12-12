e42c33626fd4c99b7a9ee5c2ed6851161d3edab8
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2024-12-12 08:54:45 +0200

    orders done

---
+++ frontend/src/pages/admin/Orders.tsx
@@ -0,0 +1,81 @@
+import useApi from "@/hooks/useApi";
+import Dashboard from "./Dashboard";
+import { Order, OrderItem } from "@/types/orders";
+
+export default function Orders() {
+  const {
+    data: orders,
+    isLoading: isFetchingOrders,
+    fetchData: fetchOrders,
+  } = useApi<Order[]>("http://localhost:3000/orders", {
+    skipFetch: false,
+  });
+
+  const { isLoading: isDeletingOrder, fetchData: deleteOrder } = useApi<void>(
+    "http://localhost:3000/orders",
+    { method: "DELETE", skipFetch: true },
+  );
+
+  const { isLoading: isAddingOrder, fetchData: addOrder } = useApi<void>(
+    "http://localhost:3000/orders",
+    { method: "POST", skipFetch: true },
+  );
+
+  const handleDeleteOrder = async (orderId: number) => {
+    try {
+      await deleteOrder({
+        method: "DELETE",
+        body: { id: orderId },
+      });
+      await fetchOrders();
+      alert("Order deleted successfully");
+    } catch (error) {
+      console.error("Failed to delete order: " + error);
+    }
+  };
+
+  const handleAddOrder = async (formData: Record<string, string | number>) => {
+    try {
+      await addOrder({
+        method: "POST",
+        body: { id: formData.userId },
+      });
+      await fetchOrders();
+    } catch (error) {
+      console.error("Failed to add order: " + error);
+    }
+  };
+
+  return (
+    <Dashboard<Order>
+      name="Orders"
+      data={orders || []}
+      isFetching={isFetchingOrders}
+      onFetch={fetchOrders}
+      onSubmit={handleAddOrder}
+      customRender={{
+        items: (items: OrderItem[]) => (
+          <div>
+            {items.map((item) => (
+              <div key={item.id}>
+                <p>Product ID: {item.productId}</p>
+                <p>Quantity: {item.quantity}</p>
+              </div>
+            ))}
+          </div>
+        ),
+        actions: (row: Order) => (
+          <button
+            onClick={() => {
+              handleDeleteOrder(row.id);
+            }}
+            key={`delete-${row.id}`}
+            className="h-full w-full text-red-500"
+          >
+            Delete
+          </button>
+        ),
+      }}
+    />
+  );
+}
