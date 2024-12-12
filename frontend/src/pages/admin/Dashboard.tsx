e37b4410970ebc28721bf34a1480b87c80d5b271
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2024-12-12 08:54:27 +0200

    update

--- frontend/src/pages/admin/Dashboard.tsx
+++ frontend/src/pages/admin/Dashboard.tsx
@@ -7,6 +7,7 @@ type FieldConfig = {
   name: string;
   label: string;
   type: string;
+  options?: string[];
   required?: boolean;
 };
 
@@ -16,7 +17,7 @@ type DashboardProps<T extends Record<string, any>> = {
   isFetching: boolean;
   onFetch: () => void;
   onSubmit: (formData: Record<string, string | number>) => void;
-  fields: FieldConfig[];
+  fields?: FieldConfig[];
   customRender?: customRender<T>;
 };
 
@@ -39,6 +40,7 @@ export default function Dashboard<T extends Record<string, any>>({
       const dataObj = Object.fromEntries(formData.entries());
       onSubmit(dataObj as Record<string, string | number>);
       onFetch();
+      formRef.current.reset();
     }
   };
 
@@ -52,17 +54,17 @@ export default function Dashboard<T extends Record<string, any>>({
       <h3 className="mb-2 h-14 w-full items-center rounded-xl border-2 border-gray-200 bg-white p-2 text-center text-2xl font-bold">
         {name}
       </h3>
-      <motion.div className="flex h-full flex-col space-y-2">
+      <motion.div className="flex h-full flex-col space-y-2 overflow-hidden">
         <motion.div
           onClick={() => setIsTablePanelOpen(true)}
-          className={`flex h-full flex-row items-center justify-center space-x-2 overflow-hidden rounded-xl border-2 border-gray-200 bg-white p-4 transition-all ${isTablePanelOpen ? "h-full cursor-default" : "h-1/6 cursor-pointer hover:bg-gray-100"}`}
+          className={`flex flex-row items-center justify-center space-x-2 overflow-hidden rounded-xl border-2 border-gray-200 bg-white p-1 transition-all ${isTablePanelOpen ? "h-full cursor-default" : "h-1/6 cursor-pointer hover:bg-gray-100"}`}
         >
           {!isTablePanelOpen && (
             <p className="w-full text-center font-bold">Click to open</p>
           )}
           <AnimatePresence>
             {isTablePanelOpen && (
-              <div className="h-fit overflow-x-hidden overflow-y-scroll border border-gray-200">
+              <div className="h-full overflow-x-hidden overflow-y-scroll border border-gray-200">
                 {!isFetching ? (
                   data.length > 0 ? (
                     <Table<T>
@@ -86,7 +88,7 @@ export default function Dashboard<T extends Record<string, any>>({
           onClick={() => setIsTablePanelOpen(false)}
           className={`flex flex-col items-center justify-center space-y-4 rounded-xl border-2 border-gray-200 bg-white transition-all ${!isTablePanelOpen ? "h-full cursor-default" : "h-1/6 cursor-pointer hover:bg-gray-100"}`}
         >
-          {!isTablePanelOpen ? (
+          {!isTablePanelOpen && fields ? (
             <>
               <h4 className="w-full p-4 text-center text-xl font-medium">
                 Add {name}
@@ -102,13 +104,33 @@ export default function Dashboard<T extends Record<string, any>>({
                     className="w-full space-x-2 rounded-xl text-center"
                   >
                     <label htmlFor={field.name}>{field.label}:</label>
-                    <input
-                      id={field.name}
-                      name={field.name}
-                      type={field.type}
-                      required={field.required}
-                      className="rounded-md border border-black bg-white p-1 text-black"
-                    />
+                    {field.type === "select-multiple" ? (
+                      <select
+                        id={field.name}
+                        name={field.name}
+                        multiple
+                        size={3}
+                        className="rounded-md border border-black bg-white p-1 text-black outline-none"
+                      >
+                        {field.options?.map((option, i) => (
+                          <option
+                            key={i}
+                            value={option}
+                            className="border-b border-gray-200 py-1"
+                          >
+                            {option}
+                          </option>
+                        ))}
+                      </select>
+                    ) : (
+                      <input
+                        id={field.name}
+                        name={field.name}
+                        type={field.type}
+                        required={field.required}
+                        className="rounded-md border border-black bg-white p-1 text-black"
+                      />
+                    )}
                   </p>
                 ))}
                 <button
@@ -119,6 +141,8 @@ export default function Dashboard<T extends Record<string, any>>({
                 </button>
               </form>
             </>
+          ) : !fields ? (
+            <p className="w-full text-center font-bold">No options provided</p>
           ) : (
             <p className="w-full text-center font-bold">Click to Open</p>
           )}
